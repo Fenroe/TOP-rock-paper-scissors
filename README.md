8df37bd99a68fd8551ad688f7fb8e5863d530732
Fenroe <scrambles9494@gmail.com>
2021-07-14 18:10:12 +0300

    updated README.md with changes to the project.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TOP-rock-paper-scissors
 JS Exercise as part of The Odin Project
+
+14/7/21 18:09
+
+The JS code has been drafted. Going forward I will work to solve the following issues:
+- Code is long and repetitive. I might want to consider moving away from else if statements or implement more functions to handle repeated tasks
+- My solution for players making their choice in capital letters isn't working, though because I accounted for invalid choices already this doesn't crash the program. 
--- script/index.js
+++ script/index.js
@@ -1,1 +1,53 @@
-console.log("Hello World");
+const gameDecisions = ["rock", "paper", "scissors"]
+
+function playerPlay() {
+    let playerDecision = window.prompt("Rock, Paper, or Scissors: Make your choice!");
+    if (gameDecisions.indexOf(playerDecision)===-1) {
+        return -1;
+    }
+    else {
+        let playerDecisionFormatted = playerDecision.toLowerCase();
+        return playerDecisionFormatted;
+    }
+}
+
+function computerPlay() {
+    let computerDecision = gameDecisions[Math.floor(Math.random() * gameDecisions.length)];
+    return computerDecision;
+}
+
+function gameRound(playerSelection, computerSelection) {
+    if (playerSelection === -1) {
+        return "Sorry, that's not a valid choice";
+    }
+    else if (playerSelection === computerSelection) {
+        return "You both chose "+playerSelection+", so it's a draw."
+    }
+    else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "The computer chose "+computerSelection+". You lose!";
+        }
+        else {
+            return "The computer chose "+computerSelection+". You win!";
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "The computer chose "+computerSelection+". You win!";
+        }
+        else {
+            return "The computer chose "+computerSelection+". You lose!";
+        }
+    }
+    else {
+        if (computerSelection === "paper") {
+            return "The computer chose "+computerSelection+". You win!";
+        }
+        else {
+            return "The computer chose "+computerSelection+". You lose!";
+        }
+    }
+}
+
+console.log(gameRound(playerPlay(), computerPlay()));
+
