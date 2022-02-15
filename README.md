9112a632c1a716e1641967213c3108a310b92134
Fenroe <scrambles9494@gmail.com>
2022-02-15 06:21:00 +0200

    updated UI class and edited readme

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
 # TOP-rock-paper-scissors
-JS Exercise as part of The Odin Project
 
-14/7/21 18:09
-
-The JS code has been drafted. Going forward I will work to solve the following issues:
-- Code is long and repetitive. I might want to consider moving away from else if statements or implement more functions to handle repeated tasks
-- My solution for players making their choice in capital letters isn't working, though because I accounted for invalid choices already this doesn't crash the program. 
-
-14/7/21 18:20
-
-The second issue has been resolved. 
-
-14/7/21 18:38
-
-The first issue has been eased to an extent. Although I wouldn't call it well-written code, I'm happy with its readability for the time being.
-
-14/7/21 19:15 
-
-I added the game function to declare a winner after either the player or computer gains five points. This was somewhat trickier than expected though I was able to keep calm and debug the code. Again, this isn't super efficient code but I'm happy to have it working in the way I envisioned. 
+This is an App that allows the user to play rock, paper, scissors against an AI. 
--- src/components/UI/UI.js
+++ src/components/UI/UI.js
@@ -1,30 +1,40 @@
 class UI {
   #content;
 
+  #header;
+
+  #main;
+
+  #footer;
+
+  #gameBoard;
+
   constructor() {
     this.#content = document.querySelector('#content');
+    this.#header = document.createElement('header');
+    this.#main = document.createElement('main');
+    this.#footer = document.createElement('footer');
+    this.#gameBoard = document.createElement('section');
   }
 
-  makeHeader() {
-    const header = document.createElement('header');
-    header.classList.add('header');
+  initHeader() {
+    this.#header.classList.add('header');
     const pageTitle = document.createElement('h1');
     pageTitle.classList.add('page-title');
     pageTitle.textContent = 'Rock, Paper, Scissors.js';
-    header.append(pageTitle);
-    this.#content.append(header);
+    this.#header.append(pageTitle);
+    this.#content.append(this.#header);
   }
 
-  makeMain() {
-    const main = document.createElement('main');
-    main.classList.add('main');
-    const gameBoard = document.createElement('section');
-    gameBoard.classList.add('game-board');
-    main.append(gameBoard);
-    this.#content.append(main);
+  initMain() {
+    this.#main.classList.add('main');
+    this.#gameBoard.classList.add('game-board');
+    this.#main.append(this.#gameBoard);
+    this.#content.append(this.#main);
   }
 
-  makePreGameBoard() {
+  renderPreGameBoard() {
+    this.#gameBoard.innerHTML = '';
     const enterNameLabel = document.createElement('label');
     enterNameLabel.classList.add('enter-name-label');
     enterNameLabel.textContent = 'Enter Your Name';
@@ -33,23 +43,95 @@ class UI {
     const startGameButton = document.createElement('button');
     startGameButton.classList.add('start-game-button');
     startGameButton.textContent = 'Start Game';
-    const gameBoard = document.querySelector('.game-board');
-    gameBoard.append(enterNameLabel);
-    gameBoard.append(enterNameInput);
-    gameBoard.append(startGameButton);
+    this.#gameBoard.append(enterNameLabel);
+    this.#gameBoard.append(enterNameInput);
+    this.#gameBoard.append(startGameButton);
   }
 
-  makeFooter() {
-    const footer = document.createElement('footer');
-    footer.classList.add('footer');
-    this.#content.append(footer);
+  initStartGameButton(callback) {
+    const button = this.#gameBoard.querySelector('.start-game-button');
+    button.addEventListener('click', () => {
+      const enterNameValue = document.querySelector('.enter-name-input').value;
+      callback(enterNameValue);
+    });
+  }
+
+  renderDecisionScreen() {
+    this.#gameBoard.innerHTML = '';
+    const rockButton = document.createElement('button');
+    rockButton.classList.add('.rock-button');
+    rockButton.dataset.key = 'rock';
+    rockButton.textContent = 'Rock';
+    const paperButton = document.createElement('button');
+    paperButton.classList.add('.paper-button');
+    paperButton.dataset.key = 'paper';
+    paperButton.textContent = 'Paper';
+    const scissorsButton = document.createElement('button');
+    scissorsButton.classList.add('.scissors-button');
+    scissorsButton.dataset.key = 'scissors';
+    scissorsButton.textContent = 'Scissors';
+    this.#gameBoard.append(rockButton);
+    this.#gameBoard.append(paperButton);
+    this.#gameBoard.append(scissorsButton);
+  }
+
+  initDecisionButtons(callback) {
+    const decisionButtons = this.#gameBoard.querySelectorAll('button');
+    decisionButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        callback(button.dataset.key);
+      });
+    });
+  }
+
+  renderResultsScreen(result) {
+    this.#gameBoard.innerHTML = '';
+    const resultHeading = document.createElement('h1');
+    resultHeading.classList.add('result-heading');
+    resultHeading.textContent = result;
+    const continueButton = document.createElement('button');
+    continueButton.classList.add('continue-button');
+    continueButton.textContent = 'Continue';
+    this.#gameBoard.append(resultHeading);
+    this.#gameBoard.append(continueButton);
+  }
+
+  initContinueButton(callback) {
+    const continueButton = this.#gameBoard.querySelector('.continue-button');
+    continueButton.addEventListener('click', () => {
+      callback();
+    });
+  }
+
+  renderGameOverScreen(result) {
+    this.#gameBoard.innerHTML = '';
+    const gameOverText = document.createElement('h1');
+    gameOverText.classList.add('game-over-text');
+    gameOverText.textContent = result;
+    const playAgainButton = document.createElement('button');
+    playAgainButton.classList.add('play-again-button');
+    playAgainButton.textContent = 'Play Again';
+    this.#gameBoard.append(gameOverText);
+    this.#gameBoard.append(playAgainButton);
+  }
+
+  initPlayAgainButton(callback) {
+    const playAgainButton = this.#gameBoard.querySelector('.play-again-button');
+    playAgainButton.addEventListener('click', () => {
+      callback();
+    });
+  }
+
+  initFooter() {
+    this.#footer.classList.add('footer');
+    this.#content.append(this.#footer);
   }
 
   renderPage() {
-    this.makeHeader();
-    this.makeMain();
-    this.makeFooter();
-    this.makePreGameBoard();
+    this.initHeader();
+    this.initMain();
+    this.initFooter();
+    this.renderPreGameBoard();
   }
 }
 
